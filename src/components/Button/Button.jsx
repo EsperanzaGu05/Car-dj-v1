0e778b63fcd464436ef634660a7c0186f1ca257d
Esperanza Gualdron <espeeranza.g.p@gmail.com>
2024-06-09 21:36:01 -0400

    first commit

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Button.css";
+
+const Button = ({ message }) => {
+  return <button className="login-button">{message}</button>;
+};
+
+export default Button;
---
+++ src/components/Images/Icon.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Icon = (props) => {
+  return (
+    <img id="logo" src={props.src} width={props.width} height={props.height} />
+  );
+};
+
+export default Icon;
