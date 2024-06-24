3c4f3f9e9a357d8ffac91609753a57f207117748
Rachit <rachitgandhi2212@gmail.com>
2024-06-24 19:18:18 -0400

    updated folder structure

--- backend_1/f/f/src/components/Images/Icon.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Icon = (props) => {
-  return (
-    <img id="logo" src={props.src} width={props.width} height={props.height} />
-  );
-};
-
-export default Icon;
--- frontend/src/components/AsideBar/AsideBar.jsx
+++ frontend/src/components/AsideBar/AsideBar.jsx
@@ -1,24 +1,25 @@
 import React from "react";
-import Icon from "../Images/Icon";
 import "../AsideBar/AsideBar.css";
 import Login from "../LoginSection/Login";
 import FeatureButton from "../Button/FeatureButton";
+
+import iconSrc from "../../assets/icon.png"; // Adjust the path based on your directory structure
+import noteSrc from "../../assets/note.png"; 
+import microphoneSrc from "../../assets/microphone.png";
+import listSrc from "../../assets/list.png";
 
 const AsideBar = () => {
   return (
     <aside>
       <h1 id="logo-side">
-        <Icon src="./src/assets/icon.png" width="30px" height="30px" />
+        <img src={iconSrc} alt="Icon" width="30px" height="30px" />
         Car DJ
       </h1>
       <Login />
       <div id="features">
-        <FeatureButton src="./src/assets/note.png" featureName="Albums" />
-        <FeatureButton
-          src="./src/assets/microphone.png"
-          featureName="Artists"
-        />
-        <FeatureButton src="./src/assets/list.png" featureName="Playlists" />
+        <FeatureButton src={noteSrc} featureName="Albums" />
+        <FeatureButton src={microphoneSrc} featureName="Artists" />
+        <FeatureButton src={listSrc} featureName="Playlists" />
       </div>
     </aside>
   );
