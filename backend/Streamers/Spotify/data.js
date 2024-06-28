0a907b31ebdf2ef191103a584c6f2c0ee2d01837
Esperanza Gualdron <espeeranza.g.p@gmail.com>
2024-06-28 11:48:51 -0400

    Home and spotify API

--- backend/Streamers/Spotify/data.js
+++ backend/Streamers/Spotify/data.js
@@ -8,30 +8,30 @@ const app = express();
 app.get('/track', (req, res) => {
     //let id='11dFghVXANMlKmJXsNCbNl';
     const { id } = req.query;
-    SpotifyConn(async(error, instance) => {
-        if(instance){
-            try{
+    SpotifyConn(async (error, instance) => {
+        if (instance) {
+            try {
                 let track = await instance.get(`/tracks/${id}?market=ES`);
-                return res.status(200).json({...track?.data});
-            }catch(error){
+                return res.status(200).json({ ...track?.data });
+            } catch (error) {
                 console.log(error);
             }
-        }else {
+        } else {
             return res.status(error?.status).json(error);
         }
     })
-} );
+});
 
-app.get('/new-releases', (req, res)=> {
-    SpotifyConn(async(error, instance) => {
-        if(instance){
-            try{
+app.get('/new-releases', (req, res) => {
+    SpotifyConn(async (error, instance) => {
+        if (instance) {
+            try {
                 let data = await instance.get(`/browse/new-releases?limit=40&offset=0`);
-                return res.status(200).json({...data?.data});
-            }catch(error){
+                return res.status(200).json({ ...data?.data });
+            } catch (error) {
                 console.log(error);
             }
-        }else{ 
+        } else {
             return res.status(error?.status).json(error);
         }
     })
@@ -40,25 +40,25 @@ app.get('/new-releases', (req, res)=> {
 // Getting artists from spotify for set of ids
 app.get('/artists', (req, res) => {
     const { ids } = req.query;
-    SpotifyConn(async(error, instance) => {
-        if(instance){
-            if(ids){
-                try{
-                     let data = await instance.get(`/artists?ids=${ids}`);
-                    return res.status(200).json({...data?.data});  
-                }catch(error){
+    SpotifyConn(async (error, instance) => {
+        if (instance) {
+            if (ids) {
+                try {
+                    let data = await instance.get(`/artists?ids=${ids}`);
+                    return res.status(200).json({ ...data?.data });
+                } catch (error) {
                     console.log(error);
                 }
-            }else{
+            } else {
                 //When ids are not given this gives the artists of the newly released albums
                 let newAlbums = await instance.get(`/browse/new-releases?limit=10&offset=0`);
                 let artArray = newAlbums.data.albums.items.map(item => item.artists).flat();
                 artArray = Array.from(new Set(artArray));
                 let idArray = artArray.map(artist => artist.id);
                 let data = await instance.get(`/artists?ids=${idArray.join(',')}`);
-                return res.status(200).json({...data?.data}); 
+                return res.status(200).json({ ...data?.data });
             }
-        }else{ 
+        } else {
             return res.status(error?.status).json(error);
         }
     })
@@ -66,15 +66,15 @@ app.get('/artists', (req, res) => {
 
 app.get('/albums', (req, res) => {
     const { ids } = req.query;
-    SpotifyConn(async(error, instance) => {
-        if(instance){
-            try{
+    SpotifyConn(async (error, instance) => {
+        if (instance) {
+            try {
                 let data = await instance.get(`/albums?ids=${ids}`);
-                return res.status(200).json({...data?.data});
-            }catch(error){
+                return res.status(200).json({ ...data?.data });
+            } catch (error) {
                 console.log(error);
             }
-        }else{ 
+        } else {
             return res.status(error?.status).json(error);
         }
     })
