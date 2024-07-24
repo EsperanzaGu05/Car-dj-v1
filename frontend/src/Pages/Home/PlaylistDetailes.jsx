77345459d45e47e44a277e5e8ef397ce03bb2256
Subhash Ghakhar <c0888601@mylambton.ca>
2024-07-24 17:08:23 -0400

    Update PlaylistDetailes.jsx

--- frontend/src/Pages/Home/PlaylistDetailes.jsx
+++ frontend/src/Pages/Home/PlaylistDetailes.jsx
@@ -115,7 +115,7 @@ const PlaylistDetails = () => {
       return;
     }
     if (!isSubscribed) {
-      showSnackbar("You need an active or recently cancelled subscription to add songs to playlists", "error");
+      showSnackbar("You need an active subscription to add songs to playlists", "error");
       return;
     }
 
